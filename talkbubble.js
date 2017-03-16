f6519c196e46b803908b59c378117e6b15cdc03f
Smittyvb <smittyvb@gmail.com>
2017-03-16 14:47:21 -0400

    allow multiple talkbubbles at once

--- talkbubble.js
+++ talkbubble.js
@@ -1,22 +1,21 @@
 (function ($) {
-  var text = "";
-  var dispText = "";
-  var currentIndex = 0;
-  var $talkBubbleEle = null;
-  var aniFrame = 50;
-  $.fn.talkBubble = function (newText) {
+  $.fn.talkBubble = function (newText, aniFrame) {
+    var text = "";
+    var dispText = "";
+    var currentIndex = 0;
+    var $talkBubbleEle = null;
     $talkBubbleEle = this;
     text = newText;
     currentIndex = 0;
     dispText = "";
+    setInterval(function () {
+      if (($talkBubbleEle === null) || (text.length === dispText.length)) {
+        return;
+      }
+      dispText += text[currentIndex];
+      currentIndex++;
+      $talkBubbleEle.text(dispText);
+      return this;
+    }, aniFrame);
   };
-  setInterval(function () {
-    if (($talkBubbleEle === null) || (text.length === dispText.length)) {
-      return;
-    }
-    dispText += text[currentIndex];
-    currentIndex++;
-    $talkBubbleEle.text(dispText);
-    return this;
-  }, aniFrame);
 })(jQuery);
